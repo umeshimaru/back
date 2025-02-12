67d9266b190f2820caa181cb53940c60231673f8
umekishimaru <riki12344649@gmail.com>
2025-02-12 16:32:41 +0900

    setup/api連携の設定をしてフロントにHello Worldを返却

--- spe-con/config/puma.rb
+++ spe-con/config/puma.rb
@@ -27,8 +27,8 @@
 threads_count = ENV.fetch("RAILS_MAX_THREADS", 3)
 threads threads_count, threads_count
 
-# Specifies the `port` that Puma will listen on to receive requests; default is 3000.
-port ENV.fetch("PORT", 3000)
+# Specifies the `port` that Puma will listenp on to receive requests; default is 3000.
+# port ENV.fetch("PORT", 3000)
 
 # Allow puma to be restarted by `bin/rails restart` command.
 plugin :tmp_restart
@@ -39,3 +39,10 @@ plugin :solid_queue if ENV["SOLID_QUEUE_IN_PUMA"]
 # Specify the PID file. Defaults to tmp/pids/server.pid in development.
 # In other environments, only set the PID file if requested.
 pidfile ENV["PIDFILE"] if ENV["PIDFILE"]
+
+
+ssl_bind '0.0.0.0', '3000', {
+  key: './localhost-key.pem', 
+  cert: './localhost.pem',
+  verify_mode: 'none'
+}
