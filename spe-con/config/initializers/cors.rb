8eff8c24f3377cb8f0c5d0bb8082115a0f075705
umekishimaru <riki12344649@gmail.com>
2025-04-05 18:14:20 +0900

    cors設定修正

--- spe-con/config/initializers/cors.rb
+++ spe-con/config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "https://localhost:8080","https://0.0.0.0:3000","https://spe-con.vercel.app","http://0.0.0.0:3000","https://back-fznr.onrender.com"
+    origins "https://localhost:8080","https://0.0.0.0:3000","https://spe-con.vercel.app/","http://0.0.0.0:3000","https://back-fznr.onrender.com"
     
     resource "*",
       headers: :any,
